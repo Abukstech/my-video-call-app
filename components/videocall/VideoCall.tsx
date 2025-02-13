bb176d12e7c5abd0f7847b594ab68c5ed96de4c3
Abukstech <abdulmajeedbamidele4@gmail.com>
2025-02-13 03:00:45 -0800

    fix the ios permissions

--- components/videocall/VideoCall.tsx
+++ components/videocall/VideoCall.tsx
@@ -8,8 +8,8 @@ import {
 import { VideoUI } from "./VideoUi";
   
   const apiKey = "mmhfdzb5evj2";
-  const userId = "Count_Dooku";
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0NvdW50X0Rvb2t1IiwidXNlcl9pZCI6IkNvdW50X0Rvb2t1IiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6NjA0ODAwLCJpYXQiOjE3MzkzNzMzODUsImV4cCI6MTczOTk3ODE4NX0.PB3Oo8DDrFg9VnXEjhm3mISrr3ZKzV-07wNn7VzJsZQ";
+  const userId = "Count_Doksss";
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVMJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0NvdW50X0Rvb2t1IiwidXNlcl9pZCI6IkNvdW50X0Rvb2t1IiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6NjA0ODAwLCJpYXQiOjE3MzkzNzMzODUsImV4cCI6MTczOTk3ODE4NX0.PB3Oo8DDrFg9VnXEjhm3mISrr3ZKzV-07wNn7VzJsZQ";
   const callId = "NX7wgsT31zDZ";
   const user: User = { id: userId };
   
